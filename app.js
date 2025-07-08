a838e3acef07dfda132b6f003cf4f2c160f2483e
LeonardoRomulo <leo.romulo.romulo15@gmail.com>
2025-07-07 21:08:05 -0300

    feat: criando a rota e a função de busca por id

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { retornaCampeonatos } from './service/service.js';
+import { retornaCampeonatos, retornaCampeonatosId } from './service/service.js';
 const app = express();
 
 app.get('/campeonatos', async (req, res) => {
@@ -8,8 +8,18 @@ app.get('/campeonatos', async (req, res) => {
 });
 
 app.get('/campeonatos/:id', async (req, res) => {
-    const id = req.params.id;
+    const id = parseInt(req.params.id);
+    const campeonatos = await retornaCampeonatosId(id);
+    res.json(campeonatos);
 });
+
+// app.get ('/campeonatso/?ano', async (req,res) => {
+
+// })
+
+// app.get('/campeonatos/?time', async (req,res) => {
+
+// })
 
 app.listen(9000, async () => {
     const date = new Date();
--- service/service.js
+++ service/service.js
@@ -10,3 +10,13 @@ export const retornaCampeonatos = async () => {
 
     return campeonatos;
 } 
+
+export const retornaCampeonatosId = async (id) => {
+    const conexao = await pool.getConnection();
+
+    const campeonatos_tb = await conexao.query('SELECT id, campeao, vice, ano FROM campeonatos WHERE id=?',[id]);
+    const campeonatos = campeonatos_tb[0];
+
+    conexao.release();
+    return campeonatos;
+}
